098ba4bf98bc9b92cef61a02e59e4fceb89e432a
Sébastien Perpignane <sebastien.perpignane@gmail.com>
2022-11-24 01:53:36 +0100

    Missing changes on the html/js files.

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,4 +1,5 @@
 var stompClient = null;
+gameId = "";
 
 function setConnected(connected) {
     $("#connect").prop("disabled", connected);
@@ -21,6 +22,57 @@ function connect() {
         stompClient.subscribe('/topic/game', function (greeting) {
             showGreeting(JSON.parse(greeting.body).content);
         });
+    });
+}
+
+function startNewGame() {
+
+    var socket = new SockJS('/stomp');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+
+
+
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        /*stompClient.subscribe('/topic/game', function (greeting) {
+            showGreeting(JSON.parse(greeting.body).content);
+        });*/
+
+        $.ajax({
+            url : 'http://localhost:8080/contree/game/create',
+            type : 'POST',
+            //dataType:'json',
+            success : function(data) {
+                gameId = data;
+                console.log("Data : " + data);
+                console.log("Game Id : " + gameId);
+
+                stompClient.subscribe('/topic/game/' + gameId, function(greeting) {
+                    showGreeting(greeting.body);
+                });
+
+                $.ajax({
+                    url: 'http://localhost:8080/contree/game/' + gameId + '/join',
+                    type: 'POST',
+                    //dataType:'json',
+                    success: function (data) {
+                        console.log("Join data:" + data);
+                    },
+                    error: function (request, error) {
+                        alert("Request: " + JSON.stringify(request));
+                    }
+                });
+
+            },
+            error : function(request,error)
+            {
+                alert("Request: "+JSON.stringify(request));
+            }
+        });
+
+
+
     });
 }
 
@@ -47,12 +99,55 @@ function subscribe() {
     });
 }
 
+function placeBid() {
+    $.ajax({
+
+        url : 'http://localhost:8080/contree/game/' +  gameId + '/place-bid',
+        type : 'POST',
+        data : JSON.stringify({
+            'playerName' : 'Seb',
+            'bidValue': $('#bid-value').val(),
+            'cardSuit': $('#bid-suit').val()
+        }),
+        contentType: 'application/json; charset=UTF-8',
+        success : function(data) {
+            console.log('Place bid data: '+data);
+        },
+        error : function(request,error)
+        {
+            alert("Request: "+JSON.stringify(request));
+        }
+    });
+}
+
+function playCard() {
+    $.ajax({
+
+        url : 'http://localhost:8080/contree/game/' + gameId + '/play-card',
+        type : 'POST',
+        data : JSON.stringify({
+            'playerName' : 'Seb',
+            'card': $('#played-card').val()
+        }),
+        contentType: 'application/json; charset=UTF-8',
+        success : function(data) {
+            console.log('Play card data: '+data);
+        },
+        error : function(request,error)
+        {
+            alert("Request: "+JSON.stringify(request));
+        }
+    });
+}
+
 $(function () {
     $("form").on('submit', function (e) {
         e.preventDefault();
     });
-    $( "#connect" ).click(function() { connect(); });
+    $( "#connect" ).click(function() { startNewGame(); });
     $( "#disconnect" ).click(function() { disconnect(); });
     $( "#send" ).click(function() { sendName(); });
     $( "#subscribe" ).click(function() { subscribe(); });
+    $( "#place-bid" ).click(function() { placeBid(); });
+    $( "#play-card" ).click(function() { playCard(); });
 });
